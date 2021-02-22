f7b8b7cc731ed95553e99efa6a81a31b590231dd
GauthierLory <gauthier.lory@protonmail.com>
2021-02-22 17:11:57 +0100

    controllers + routers

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
+const userRoutes = require('./user.routes');
+const authRoutes = require('./auth.routes');
 const router = require('express').Router();
+
+router.get('/users', userRoutes);
+router.get('/auth', authRoutes);
 
 router.get('/',  (req, res) => {
     res.render('index');
