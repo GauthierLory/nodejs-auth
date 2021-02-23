3c0700a813361cf855ee42e9319d6d7846c6cc38
GauthierLory <gauthier.lory@protonmail.com>
2021-02-23 15:09:29 +0100

    passport & session login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,12 @@
 const { sessionNew, sessionCreate, sessionDelete } = require('../controllers/auth.controller');
 const router = require('express').Router();
+const passport = require('passport')
 
 router.get('/signin/form', sessionNew);
-router.post('/signin', sessionCreate);
+router.post('/signin', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/auth/signin/form'
+}));
 router.post('/logout', sessionDelete);
 
 module.exports = router;
