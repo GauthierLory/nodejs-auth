de679f3165241d620a6c9bc72706a2568c81e2f1
GauthierLory <gauthier.lory@protonmail.com>
2021-02-23 19:03:01 +0100

    google OAuth2 + routes & controllers

--- config/passport.config.js
+++ config/passport.config.js
@@ -2,7 +2,9 @@ const passport = require('passport');
 const { app } = require('../app');
 const User = require('../database/models/user.model');
 const LocalStrategy = require('passport-local').Strategy;
+const googleStrategy = require('passport-google-oauth20').Strategy;
 const { findUserPerEmail } = require('../queries/user.queries')
+const util = require('util');
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -36,4 +38,13 @@ passport.use('local',new LocalStrategy({ usernameField: 'email'},async (email, p
     }catch (e) {
         done(e);
     }
+}))
+
+passport.use('google', new googleStrategy({
+    clientID: '679321079910-5cgul7ra24hecodjo8se0nrt2bs47u64.apps.googleusercontent.com',
+    clientSecret: 'y-7Nknd_3vouQVkUO1hqb1fl',
+    callbackURL: '/auth/google/cb'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(util.inspect(profile, { compact: true, depth: 5, breakLength: 80 }))
+    done(null, false, { message: "test" });
 }))
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,11 @@
-const { sessionNew, sessionCreate, sessionDelete } = require('../controllers/auth.controller');
+const { sessionNew, sessionCreate, sessionDelete, googleAuth, googleAuthCb } = require('../controllers/auth.controller');
 const router = require('express').Router();
 const passport = require('passport')
 
 router.get('/signin/form', sessionNew);
 router.post('/signin', sessionCreate);
 router.get('/signout', sessionDelete);
+router.get('/google', googleAuth)
+router.get('/google/cb', googleAuthCb)
 
 module.exports = router;
