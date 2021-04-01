97c9dacb89c27c6f3d6cd30ba8da37f89c63f996
GauthierLory <gauthier.lory@protonmail.com>
2021-04-01 17:21:56 +0200

    mise en place i18n

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 require('./database');
+const i18n = require('i18n')
 const router = require('./routes');
 const app = express();
 
@@ -9,11 +10,19 @@ exports.app = app;
 require('./config/session.config');
 require('./config/passport.config')
 
+
+i18n.configure({
+    locales: ['fr', 'en'],
+    directory: path.join(__dirname, 'locales'),
+    updateFiles: false,
+})
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(i18n.init)
 app.use(router);
 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const userRoutes = require('./user.routes');
 const authRoutes = require('./auth.routes');
+// const i18n = require('i18n');
 
 router.use('/users', userRoutes);
 router.use('/auth', authRoutes);
 
 router.get('/',  (req, res) => {
+    // i18n.setLocale(req, req.params.lang)
     res.render('index', { user: req.user });
   });
 
